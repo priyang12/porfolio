c4daa8037a78f64b012a9e8262cc004d8066bfa8
Priyang <patelpriyang95@gmail.com>
2025-07-15 18:57:07 +0530

    hero sections

--- app/Components/Navbar/Navbar.tsx
+++ app/Components/Navbar/Navbar.tsx
@@ -3,12 +3,14 @@ import { clsx } from "clsx";
 
 function LogoComponent() {
   return (
-    <Link
-      to="/"
-      className="btn-ghost btn w-full text-center text-3xl normal-case sm:w-fit"
-    >
-      Priyang Patel
-    </Link>
+    <div className="col-span-12 md:col-end-4 md:col-start-2">
+      <Link
+        to="/"
+        className="block w-full text-2xl md:text-3xl text-center md:text-start"
+      >
+        Priyang Patel
+      </Link>
+    </div>
   );
 }
 
@@ -16,24 +18,24 @@ function Navbar() {
   return (
     <div
       className={clsx(
-        `glass-container sticky top-0 left-0 flex z-20 flex-wrap rounded-b-md p-5 items-center`
+        `glass-container h=[20vh] py-5 gap-5 grid grid-cols-12 sticky top-0 left-0 z-20 flex-wrap rounded-b-md items-center`
       )}
     >
       <LogoComponent />
 
-      <ul className="flex justify-between m-auto sm:ml-auto sm:mr-0 flex-row p-0">
-        <li>
-          <Link to="/Projects" className="p-2 text-xl normal-case">
+      <ul className="col-span-12 md:col-start-7 md:col-end-12 grid grid-rows-subgrid grid-cols-3 items-center">
+        <li className="col-span-1 m-auto">
+          <Link to="/Projects" className="py-2 px-1 text-xl normal-case">
             Projects
           </Link>
         </li>
-        <li>
-          <Link to="/Blogs" className="p-2 text-xl normal-case">
+        <li className="col-span-1 m-auto">
+          <Link to="/Blogs" className="py-2 px-1 text-xl normal-case">
             Blogs
           </Link>
         </li>
-        <li>
-          <Link to="/AboutMe" className="p-2 text-xl normal-case">
+        <li className="col-span-1 m-auto">
+          <Link to="/AboutMe" className="py-2 px-1 text-xl normal-case">
             About Me
           </Link>
         </li>
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,18 +1,51 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { Description, Subtitle, Title } from "../../content/Hero.json";
+import { Github, Twitter } from "../../content/Socials.json";
+import { FiGithub, FiTwitter } from "react-icons/fi";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Home/" },
+    { name: "description", content: "This is my Personal portfolio" },
   ];
 }
 
+const HeroContainer = () => {
+  return (
+    <section className="col-start-2 col-end-12 flex items-center min-h-[80vh]">
+      <div className="mx-auto text-center">
+        <header className="font-VT323">
+          <h1 className="text-5xl sm:text-7xl text-primary">{Title}</h1>
+          <h2 className="my-4 text-3xl sm:text-5xl text-primary">
+            {Subtitle} 👋
+          </h2>
+          <p className="mt-4 text-xl sm:text-3xl text-primary">{Description}</p>
+        </header>
+
+        <div className="mt-8 flex flex-col items-center gap-4 sm:flex-row sm:justify-center font-Roboto">
+          <a href={Github}>
+            <button className="flex items-center cursor-pointer gap-2 px-4 py-2 border rounded hover:bg-primary hover:text-white transition">
+              <FiGithub className="text-xl" />
+              Github
+            </button>
+          </a>
+          <a href={Twitter}>
+            <button className="flex items-center cursor-pointer gap-2 px-4 py-2 border rounded hover:bg-primary hover:text-white transition">
+              <FiTwitter className="text-xl" />
+              Twitter
+            </button>
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
 export default function Home() {
   return (
-    <>
-      <Welcome />
-      <h1>adasd</h1>
-    </>
+    <div className="h-[80vh] gap-5 grid grid-cols-12">
+      {/* this will moved to about me page. */}
+      <HeroContainer />
+    </div>
   );
 }
