e7abb82aa7a6f2ee09b9b6b8b4eee30f82997b36
Priyang <patelpriyang95@gmail.com>
2025-07-27 12:26:50 +0530

    removing caching during dev

--- app/Utils/cache.server.ts
+++ app/Utils/cache.server.ts
@@ -5,11 +5,20 @@ declare global {
   var __cache: NodeCache | undefined;
 }
 
+class MockDevCache {
+  get() {
+    return undefined;
+  }
+  set() {
+    return true;
+  }
+}
+
 if (process.env.NODE_ENV === 'production') {
   cache = new NodeCache();
 } else {
   if (!global.__cache) {
-    global.__cache = new NodeCache();
+    global.__cache = new MockDevCache() as unknown as NodeCache;
   }
   cache = global.__cache;
 }
